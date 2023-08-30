769220e90ec73be20a1cbc405d93c229647f5fed
BahruDa <bahrudari68@gmail.com>
2023-08-30 18:24:03 +0300

    hot fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,75 +1,85 @@
 #!/usr/bin/env python3
-"""Personal data"""
-from typing import List
-import re
+
+""" Filtered logger """
+
+
 import logging
 import os
+import re
+from typing import List
 import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str],
-                 redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    '''returns the log message obfuscated'''
-    for pii in fields:
-        message = re.sub(fr'{pii}=.+?{separator}',
-                         f'{pii}={redaction}{separator}', message)
+    """ Replacing """
+
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ RedactingFormatter class. """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        '''constructor'''
+        """ Initialization of class """
+
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        '''to filter values in incoming log records'''
+        """ Formating method """
+
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
-    '''returns a logging.Logger object'''
-    logObj = logging.getLogger("user_data")
-    logObj.setLevel(logging.INFO)
-    logObj.propagate = False
-    Handle = logging.StreamHandler()
-    Handle.setFormater(RedactingFormatter(list(PII_FIELDS)))
-    logObj.addHandler(Handle)
-    return logObj
+    """ A method that implements a logger """
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    '''returns a connector to the database'''
-    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
-    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
-    host = os.getenv("PERSONAL_DATA_DB_HOST")
-    dbname = os.getenv("PERSONAL_DATA_DB_NAME")
-    return mysql.connector.connect(user=username, password=password,
-                                   host=host, database=dbname)
+    """ A method that implements db conectivity """
+
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
 
 
-def main():
-    '''retrieve & display all rows in the users table'''
-    conn = get_db()
+def main() -> None:
+    """ A method that implements a main function """
+
+    db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT * from users")
-    fields = [user[0] for user in cursor.description]
-    print(fields)
-    log = get_logger()
-    for c in cursor:
-        row = ''.join(f'{f}={str(r)}; ' for r, f in zip(c, fields))
-        log.info(c)
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
     cursor.close()
     db.close()
 
